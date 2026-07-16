9d0aba87f0494a1c05ab362a652ece4dda5d1fbc
iOS SDP Mac94 <sdcuser@VY-7F-IOSTC-077.local>
2026-07-16 12:10:31 +0530

    Finished Introduction

--- Student Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Student Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("Don't Stop Me Now")
+print("QUEEN")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("I'm gonna have myself a real good time")
+print("I feel alive")
+print("And the world, I'll turn it inside out, yeah")
 /*:
 
  _Copyright © 2023 Apple Inc._
