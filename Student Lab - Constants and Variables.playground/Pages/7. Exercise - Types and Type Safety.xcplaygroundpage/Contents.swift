676feb5b6342e2fbbd2d9815efc3cc874fe9150f
iOS SDP Mac94 <sdcuser@VY-7F-IOSTC-077.local>
2026-07-16 12:39:26 +0530

    Finished Constants and Variables

--- Student Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Student Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,17 +3,20 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
-
-
+var firstDecimal: Double
+var secondDecimal: Double
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var trueOrFalse: Bool
+//firstDecimal = trueOrFalse
+print("above line will not compile because you are trying to assign a Bool to a Double")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var string: String
+//firstDecimal = string
+print("above line will not compile because you are trying to assign a String to a Double")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var num = 7
+//firstDecimal = num
+print("above line will not compile because you are trying to assign an Int to a Double")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
  */
