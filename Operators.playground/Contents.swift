6af62f0d643c77b9801e5dc6097737a8a51d334d
iOS SDP Mac94 <sdcuser@VY-7F-IOSTC-077.local>
2026-07-21 11:46:11 +0530

    Operators in swift

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,23 @@
+var a = 10
+a = 20
+
+var b: Double = 10 / 3
+var c = 4 * 5
+
+var d = b - Double(c)
+
+d += 1
+d -= 1
+d /= 2
+d *= -5
+
+var num0 = "10"
+var num1 = Int(num0) ?? 0
+var num2 = 20
+var num3 = num1 + num2
+print(num1)
+
+let x = 3
+let y = 0.141595
+let pi = Double(x) + y
+print(pi)
