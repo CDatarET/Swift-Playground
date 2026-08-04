c3f448082ce67dc928daf2a9d5657eed842b3396
CDatarET <datar.chaitanya@gmail.com>
2026-08-04 15:36:31 +0530

    .

--- Student Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
+++ Student Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -12,14 +12,24 @@ struct GPS{
 var someplace = GPS()
 print("Latitude: \(someplace.latitude)\nLongitude: \(someplace.longitude)")
 //:  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
-
-
+someplace.latitude = 51.514004
+someplace.longitude = 0.125226
+print("Latitude: \(someplace.latitude)\nLongitude: \(someplace.longitude)")
 //:  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
-
-
+struct Book{
+    var title = ""
+    var author = ""
+    var pages = 0
+    var price = 0.0
+}
 //:  Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then, using the properties of `favoriteBook`, print out facts about the book.
-
-
+var favoriteBook = Book()
+print(favoriteBook.title)
+favoriteBook.title = "The Blackthorn Key"
+favoriteBook.author = "Kevin Sands"
+favoriteBook.pages = 371
+favoriteBook.price = 1685
+print(favoriteBook)
 /*:
 page 1 of 10  |  [Next: App Exercise - Workout Tracking](@next)
  */
